354d7b43e60758e08f0308c777e7bcdf03b6142c
Oleksandr <45110010+Oleksandr-Karnaushenko@users.noreply.github.com>
2024-09-08 11:24:11 +0200

    task-5

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,5 +1,9 @@
 import createHttpError from 'http-errors';
-import { getAllProducts, getProductById } from '../services/products.js';
+import {
+  createProduct,
+  getAllProducts,
+  getProductById,
+} from '../services/products.js';
 
 export const getAllProductsController = async (req, res) => {
   const products = await getAllProducts();
@@ -18,6 +22,15 @@ export const getProductByIdController = async (req, res) => {
   res.json({
     status: 200,
     message: `Successfully found product with id ${productId}!`,
+    data: product,
+  });
+};
+export const postProductController = async (req, res) => {
+  const product = await createProduct(req.body);
+
+  res.json({
+    status: 201,
+    message: 'Successfully created a product!',
     data: product,
   });
 };
--- src/routers/products.js
+++ src/routers/products.js
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import {
   getAllProductsController,
   getProductByIdController,
+  postProductController,
 } from '../controllers/products.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 const router = Router();
 
 router.get('/products', ctrlWrapper(getAllProductsController));
 router.get('/products/:productId', ctrlWrapper(getProductByIdController));
+router.post('/products', ctrlWrapper(postProductController));
 
 export default router;
